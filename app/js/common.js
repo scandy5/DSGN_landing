bf363719dddc5b4624f746e95ea162558bc349f0
Sergey <scandy_boss@ukr.net>
2018-10-25 22:52:19 +0300

    menu

--- app/js/common.js
+++ app/js/common.js
@@ -7,5 +7,5 @@ $(function() {
 function openMenu() { 
 	document.getElementById('sidebar').classList.toggle('active');
 	document.getElementById('toggle-btn').classList.toggle('active-btn');
-	document.getElementById('toggle-btn').classList.remove('toggle-btn');
+	document.getElementById('toggle-btn').classList.toggle('toggle-btn');
 }
