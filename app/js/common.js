f04c8fca3353885e4477b2931aaeb01337026cd4
Sergey <scandy_boss@ukr.net>
2018-11-03 11:04:16 +0200

    #23 fixed error. Deleted jquery symbol

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,5 @@
-$(function() {
 
 
-
-});
-
-// function openMenu() { 
-// 	document.getElementById('sidebar').classList.toggle('active');
-// 	document.getElementById('toggle-btn').classList.toggle('active-btn');
-// 	document.getElementById('toggle-btn').classList.toggle('toggle-btn');
-// }
 
 function openMenu() {
 	document.getElementById('sidebar').classList.toggle('is-active');
